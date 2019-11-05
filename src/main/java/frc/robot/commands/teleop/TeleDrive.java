dcef06b043a9193b691031c980b8399aae7637f7
NWS Robotics <robotics@northwoodschool.us>
2019-11-05 16:06:19 -0500

    renumbered motor controllers

--- src/main/java/frc/robot/commands/teleop/TeleDrive.java
+++ src/main/java/frc/robot/commands/teleop/TeleDrive.java
@@ -36,8 +36,8 @@ public class TeleDrive extends Command {
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		fwd = -controller.getY(Hand.kLeft);
-		strafe = controller.getX(Hand.kLeft);
+		fwd = OI.deadBand(-controller.getY(Hand.kLeft));
+		strafe = OI.deadBand(controller.getX(Hand.kLeft));
 		rotateCW = OI.deadBand(controller.getX(Hand.kRight));
 
 		drivetrain.drive(fwd, strafe, rotateCW);
