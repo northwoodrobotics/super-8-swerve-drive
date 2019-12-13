ae0ebf5a73d9b6c71227c9a8509325d124a68531
northwoodrobotics-user <43829148+northwoodrobotics-user@users.noreply.github.com>
2019-12-13 13:55:40 -0500

    re-numbered motors

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -14,73 +14,68 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
+	// For example to map the left and right motors, you could define the
+	// following variables to use with your drivetrain subsystem.
+	// public static int leftMotor = 1;
+	// public static int rightMotor = 2;
 
-  //Motor Map
-  // I = inverse, S = steer
+	// Motor Map
+	// I = inverse, S = steer
 
-  //Northwest Wheel 
-  public static int northwest = 1;
-  public static int northwestS = 2;
-  public static boolean northwestI = false;
-  public static boolean northwestSI = false;
-  
-  //Northeast Wheel
-  public static int northeast = 3;
-  public static int northeastS = 4;
-  public static boolean northeastI = true;
-  public static boolean northeastSI = false;
-  
-  //Southeast Wheel
-  public static int southeast = 5;
-  public static int southeastS = 6;
-  public static boolean southeastI = true;
-  public static boolean southeastSI = false;
-  
-  //Southwest Wheel
-  public static int southwest = 7;
-  public static int southwestS = 8;
-  public static boolean southwestI = false;
-  public static boolean southwestSI = false;
-  
-  //SECOND SWERVE WHEELBASE 
+	// Northwest Wheel
+	public static int northwestS = 15;
+	public static int northwest = 16;
+	public static boolean northwestI = false;
+	public static boolean northwestSI = false;
 
-  //West Wheel
-  public static int west = 9;
-  public static int westS = 10;
-  public static boolean westI = false;
-  public static boolean westSI = false;
-  
-  //North Wheel 
-  public static int north = 11;
-  public static int northS = 12;
-  public static boolean northI = true;
-  public static boolean northSI = false;
-  
-  //East wheel
-  public static int east = 13;
-  public static int eastS = 14;
-  public static boolean eastI = true;
-  public static boolean eastSI = false;
-  
-  //South Wheel
-  public static int south = 15;
-  public static int southS = 16;
-  public static boolean southI = false;
-  public static boolean southSI = false;
+	// Northeast Wheel
+	public static int northeastS = 3;
+	public static int northeast = 4;
+	public static boolean northeastI = true;
+	public static boolean northeastSI = false;
 
+	// Southeast Wheel
+	public static int southeastS = 7;
+	public static int southeast = 8;
+	public static boolean southeastI = true;
+	public static boolean southeastSI = false;
 
-  
+	// Southwest Wheel
+	public static int southwestS = 11;
+	public static int southwest = 12;
+	public static boolean southwestI = false;
+	public static boolean southwestSI = false;
 
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
+	// SECOND SWERVE WHEELBASE
 
+	// West Wheel
+	public static int westS = 13;
+	public static int west = 14;
+	public static boolean westI = false;
+	public static boolean westSI = false;
 
+	// North Wheel
+	public static int northS = 1;
+	public static int north = 2;
+	public static boolean northI = true;
+	public static boolean northSI = false;
 
+	// East wheel
+	public static int eastS = 5;
+	public static int east = 6;
+	public static boolean eastI = true;
+	public static boolean eastSI = false;
+
+	// South Wheel
+
+	public static int southS = 9;
+	public static int south = 10;
+	public static boolean southI = false;
+	public static boolean southSI = false;
+
+	// If you are using multiple modules, make sure to define both the port
+	// number and the module. For example you with a rangefinder:
+	// public static int rangefinderPort = 1;
+	// public static int rangefinderModule = 1;
 
 }
